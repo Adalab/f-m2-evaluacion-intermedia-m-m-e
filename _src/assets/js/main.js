d6523b660df89946808fb34cd94b555e6a50a6cd
Megan Edwards <megmyfedwards@gmail.com>
2019-04-08 08:39:10 +0200

    Add constants and random number function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('>> Ready :)');
+//constants
+const counter = document.querySelector('.counter');
+const guess = document.querySelector('#guess');
+const button = document.querySelector('.button');
+const message = document.querySelector('.message');
+
+//function - random number (randNum)
+function randNum(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+console.log(randNum(100));
+
+//function - get guess and print to console (getGuess)
+
+//function - compare guess to random number 
+//(save a random number in a constant to compare)
+//then print a message on the screen (too high/too low/correct)
+
+//function - keep count of number of attempts
+
