c993b75a638b3bf4f6ae4e291e5f48a6523ae64b
Megan Edwards <megmyfedwards@gmail.com>
2019-04-08 09:00:49 +0200

    add Compare function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,22 +2,42 @@
 
 //constants
 const counter = document.querySelector('.counter');
-const guess = document.querySelector('#guess');
+const guess = document.querySelector('.guess');
 const button = document.querySelector('.button');
 const message = document.querySelector('.message');
+
+
 
 //function - random number (randNum)
 function randNum(max) {
     return Math.ceil(Math.random() * max);
   }
 
-console.log(randNum(100));
-
-//function - get guess and print to console (getGuess)
-
-//function - compare guess to random number 
-//(save a random number in a constant to compare)
+//function get guess and print to console
+//compare guess to random number 
 //then print a message on the screen (too high/too low/correct)
+
+function compareGuess(){
+    const guessInt = parseInt(guess.value);
+    console.log('guess: ' + guessInt);
+    
+    if (guessInt === rndm) {
+        message.innerHTML = `¡HAS
+        GANADO, CAMPEONA!`;
+    }
+    else if (guessInt < rndm) {
+        message.innerHTML = 'demasiado bajo';
+    }
+    else if (guessInt > rndm) {
+        message.innerHTML = 'demasiado alto';
+    }
+}
 
 //function - keep count of number of attempts
 
+//add listeners
+//(save a random number in a constant to compare)
+const rndm = randNum(100);
+console.log('random number:' + rndm);
+
+button.addEventListener('click', compareGuess);
